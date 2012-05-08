fe3ceb603443d8ce23440076e936d234cf40e14c
Frank Erens <frank@synthi.net>
2012-05-08 18:06:03 +0200

    Updated ssh-tunnel to use any server for tunnel.

--- .zsh/plugins/ssh-tunnel.zsh
+++ .zsh/plugins/ssh-tunnel.zsh
@@ -3,10 +3,20 @@
 ssh-tunnel() {
     if echo "$1" | grep -q :; then
         host=$(echo "$1" | cut -f1 -d:) 
-        port=$(echo "$1" | cut -f2 -d:) 
+        port=$(echo "$1" | cut -f2 -d:)
+        shift
     else
         host="$1"
         port="$2"
+        shift
+        shift
     fi
-    ssh -f seysayux.net -L "$port":"$host":"$port" -N
+
+    if [ -z "$1" ]; then
+        tunnel=seysayux.net
+    else
+        tunnel="$1"
+    fi
+
+    ssh -f "$tunnel" -L "$port":"$host":"$port" -N
 }
