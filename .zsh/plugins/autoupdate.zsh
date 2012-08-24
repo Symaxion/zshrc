858b080e86b55390c0188de85e1f0ce54df889c1
Frank Erens <frank@synthi.net>
2012-08-24 11:57:00 +0200

    Prevent autoupdater from running on OS X when logged in through ssh.

--- .zsh/plugins/autoupdate.zsh
+++ .zsh/plugins/autoupdate.zsh
@@ -1,3 +1,5 @@
-# actual autoupdate caller
-
-( zsh ~/.zsh/plugins/autoupdate-impl.sh & )
+# Actual autoupdate caller
+# Do not run this when logged in through ssh on OS X (keychain)
+if [ $(uname -s) != "Darwin" -o -z "$SSH_CLIENT" -a -z "$SSH_TTY" ]; then
+    ( zsh ~/.zsh/plugins/autoupdate-impl.sh & )
+fi
