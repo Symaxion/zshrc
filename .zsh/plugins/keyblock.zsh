8186d101fd31be610585ae3be037dc82882daf74
Frank Erens <frank@synthi.net>
2012-09-16 22:15:25 +0200

    Keyblock (ins,del,pgup,pgdn,...) keybindings.

---
+++ .zsh/plugins/keyblock.zsh
@@ -0,0 +1,6 @@
+# Fix the navigation keyblock.
+bindkey "^[[3~" delete-char
+bindkey "^[[H" beginning-of-line
+bindkey "^[[F" end-of-line
+bindkey "^[[5~" beginning-of-history
+bindkey "^[[6~" end-of-history
