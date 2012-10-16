b47ba8b2c715d60e966b50fe6ab4f6cea6d1ac7c
Frank Erens <frank@synthi.net>
2012-10-16 10:05:08 +0200

    Got rid of autoupdate.

--- .zsh/plugins/autoupdate.zsh
+++
@@ -1,5 +0,0 @@
-# Actual autoupdate caller
-# Do not run this when logged in through ssh on OS X (keychain)
-if [ $(uname -s) != "Darwin" -o -z "$SSH_CLIENT" -a -z "$SSH_TTY" ]; then
-    ( zsh ~/.zsh/plugins/autoupdate-impl.sh & )
-fi
